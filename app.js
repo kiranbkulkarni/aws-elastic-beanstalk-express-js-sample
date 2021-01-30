fb75f5815d9a53c267fabaf3aa00a4748b1cb8a1
Kiran Kulkarni <kiran.bkulkarni@ucdconnect.ie>
2021-01-30 13:47:39 +0000

    Full pipeline test

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Hello World Kiran, You are awesome for studying the DevOps AWS!'));
+app.get('/', (req, res) => res.send('Hello World Kiran, You are always awesome for studying the DevOps AWS!'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
